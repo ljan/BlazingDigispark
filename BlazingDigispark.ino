aca892d3779cd667db14a6f941a5cfd52dea9afe
ljan <schick.ne.email@gmail.com>
2017-10-04 11:11:46 +0200

    get currentMillis only once per loop

--- BlazingDigispark.ino
+++ BlazingDigispark.ino
@@ -1,32 +1,29 @@
 unsigned long randtime = millis();
 unsigned long fadetime = millis();
 int fadeAmount = 1;
-int brightness = 128;
-int LEDpin = 1;  //LED on Model A 
+int brightness = 128;   // Brightness at start
+int LEDpin = 1;         // LED on Model A 
 
 // the setup routine runs once when you press reset:
-void setup()
-{
+void setup() {
   pinMode(LEDpin, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
-void loop()
-{
-  if (randtime <= millis())
-  {
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if (randtime <= currentMillis) {
     fadeAmount = -fadeAmount;
-    randtime = millis() + random(100, 1000);
+    randtime = currentMillis + random(100, 1000);
   }
 
-  if (fadetime <= millis())
-  {
-    if (brightness <= 0 || brightness >= 255)
-    {
+  if (fadetime <= currentMillis) {
+    if (brightness <= 0 || brightness >= 255) {
       fadeAmount = -fadeAmount;
     }
     brightness = brightness + fadeAmount;
-    fadetime = millis() + 2;
+    fadetime = currentMillis + 2;
   }
 
   analogWrite(LEDpin, brightness);
